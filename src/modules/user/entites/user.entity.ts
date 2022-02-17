66edf3d04cf8ab7731bfc46bda37803aa07f5dcc
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-02-17 23:09:22 +0300

    user entity created

--- src/modules/user/entites/user.entity.ts
+++ src/modules/user/entites/user.entity.ts
@@ -1,5 +1,7 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+export type UserRole = 'student' | 'teacher';
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -8,8 +10,17 @@ export class User {
   @Column()
   firstName: string;
 
+  @Column({ unique: true })
+  email: string;
+
   @Column()
   lastName: string;
+
+  @Column()
+  schollId: string;
+
+  @Column()
+  role: UserRole;
 
   @Column({ default: true })
   isActive: boolean;
