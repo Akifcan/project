b9e76f70f4693e8a31516aea0bf5e2503614e2a9
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-02-20 14:46:05 +0300

    profile photo column added

--- src/modules/user/entites/user.entity.ts
+++ src/modules/user/entites/user.entity.ts
@@ -22,6 +22,12 @@ export class User {
   @Column()
   role: UserRole;
 
+  @Column({
+    default:
+      'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg',
+  })
+  profilePhoto: string;
+
   @Column({ default: true })
   isActive: boolean;
 }
