1b3ea9890d1d4736c3665db1fa0606c459912d64
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-02-26 13:21:17 +0300

    jwt integrated

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UserModule } from '../user/user.module'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule } from 'src/config/config.module'
+import { ConfigService } from 'src/config/config.service'
+
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [UserModule]
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.auth.jwt,
+        signOptions: { expiresIn: '60s' },
+      })
+    }),
+
+  ]
 })
 export class AuthModule { }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,18 +2,22 @@ import { Inject, Injectable, UnauthorizedException } from '@nestjs/common'
 import LanguageProps from 'src/common/i18y/language.interface'
 import { UserService } from '../user/user.service'
 import SignInDto from './dtos/sign-in.dto'
+import { JwtService } from '@nestjs/jwt'
+
 
 @Injectable()
 export class AuthService {
 
     @Inject() private userService: UserService
+    @Inject() private jwtService: JwtService
+
 
     async signIn(signInDto: SignInDto, language: LanguageProps) {
         const user = await this.userService.findUser(signInDto.email, signInDto.password)
         if (!user) {
             throw new UnauthorizedException(language.tr.userNotFound(signInDto.email))
         }
-        return user
+        return this.jwtService.sign({ id: user.id })
     }
 
 }
