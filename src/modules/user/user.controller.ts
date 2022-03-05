469591a22d832367ac4c1c0700340e169f763691
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-03-05 13:30:29 +0300

    profile endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject, UseGuards } from '@nestjs/common'
+import { Controller, Get, Inject, Param, UseGuards } from '@nestjs/common'
 import { User } from '../../common/decorators/user.decorator'
 import RoleGuard from '../../common/guards/role.guard'
 import CurrentUserProps from '../auth/interface/currenetUser.interface'
@@ -12,6 +12,11 @@ export class UserController {
     @Get('/me')
     me(@User() user: CurrentUserProps) {
         return this.userService.me(user.id)
+    }
+
+    @Get("profile/:id")
+    profile(@Param() params: { id: number }) {
+        return this.userService.me(params.id)
     }
 
     @UseGuards(new RoleGuard(['student', 'teacher']))
