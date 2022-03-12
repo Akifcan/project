d923984707eb54d32168a45d0f8a9f17f8669a5a
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-03-12 13:40:49 +0300

    grammer edited

--- src/common/i18y/en.ts
+++ src/common/i18y/en.ts
@@ -1,4 +1,4 @@
 export default {
-    userNotFound: (email: string) => `Sorry we can't found this email: ${email}`,
+    userNotFound: (email: string) => `Sorry we can't found this email: ${email}.`,
     unexceptedError: "An Unexcepted error occured could you try again please ?"
 }
--- src/common/i18y/tr.ts
+++ src/common/i18y/tr.ts
@@ -1,4 +1,4 @@
 export default {
-    userNotFound: (email: string) => `${email} adresi ile bir kullanıcı bulamadık`,
+    userNotFound: (email: string) => `${email} adresi ile bir kullanıcı bulamadık.`,
     unexceptedError: "Beklenmedik bir hata meydana geldi lütfen tekrar dener misiniz ?"
 }
