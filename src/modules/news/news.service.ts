982c1738587812a916d9e215b44fb5d34eb1b41f
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-03-22 15:44:00 +0300

    news scraped

--- src/modules/news/news.service.ts
+++ src/modules/news/news.service.ts
@@ -1,6 +1,6 @@
 import { HttpService } from '@nestjs/axios'
 import { Inject, Injectable } from '@nestjs/common'
-import jsdom from "jsdom"
+import { JSDOM } from "jsdom"
 import { lastValueFrom } from 'rxjs'
 
 @Injectable()
@@ -9,8 +9,26 @@ export class NewsService {
 
     async getLastNews() {
         const response = await lastValueFrom(this.httpService.get('https://haber.yasar.edu.tr/'))
-        console.log(response)
+        const dom = new JSDOM(response.data)
+        const newsBase = [...dom.window.document.querySelectorAll(".tdb_module_loop_2")]
+        const result = newsBase.map(item => {
+            const redirectLink = item.querySelector(".td-module-thumb a").getAttribute("href")
+            const thumbnail = item.querySelector(".entry-thumb").getAttribute("data-img-url")
+            const categoryRedirectLink = item.querySelector(".td-post-category").getAttribute("href")
+            const categoryName = item.querySelector(".td-post-category").textContent
+            const title = item.querySelector(".td-module-title").textContent
 
+            return {
+                title,
+                redirectLink,
+                thumbnail,
+                categoryRedirectLink,
+                categoryName,
+            }
+        })
+        console.log(JSON.stringify(result))
+
+        return result
     }
 
 }
